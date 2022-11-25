beb3f098f6ac00ab3d91c9207029975bc02bf11f
Nikita Panyuhin <n.panuhin@mail.ru>
2022-11-25 21:49:56 +0300

    Added initialization, cache dump and reset

--- src/cache.sv
+++ src/cache.sv
@@ -1,3 +1,20 @@
+class CacheLine;
+  reg valid = 0;
+  reg dirty = 1;
+  reg[CACHE_TAG_SIZE-1:0] tag;
+  reg[CACHE_LINE_SIZE-1:0] data;
+
+  function new();
+    this.reset();
+  endfunction
+
+  function void reset();
+    this.valid = 0;
+    this.dirty = 0;
+  endfunction
+endclass
+
+
 module Cache (
   input wire CLK,
   inout wire[ADDR1_BUS_SIZE-1:0] A1,
@@ -6,9 +23,40 @@ module Cache (
   inout wire[ADDR2_BUS_SIZE-1:0] A2,
   inout wire[DATA2_BUS_SIZE-1:0] D2,
   inout wire[CTR2_BUS_SIZE-1 :0] C2,
-  input wire RESET
+  input wire RESET,
+  input wire C_DUMP
 );
-  always @(RESET) begin
+  CacheLine sets [0:CACHE_SETS_COUNT] [0:CACHE_WAY];  // Total 32 * 2 = 64 cache lines (CACHE_LINE_COUNT)
+  // integer set_iterator, line_iterator;
+  CacheLine tmp_set [0:CACHE_WAY];
+  CacheLine tmp_line;
 
+  initial begin
+    for (int set_iterator = 0; set_iterator < CACHE_SETS_COUNT; set_iterator = set_iterator + 1) begin
+      for (int line_iterator = 0; line_iterator < CACHE_WAY; line_iterator = line_iterator + 1) begin
+        sets[set_iterator][line_iterator] = new ();
+      end
+    end
+  end
+
+  always @(posedge C_DUMP) begin
+    for (int set_iterator = 0; set_iterator < CACHE_SETS_COUNT; set_iterator = set_iterator + 1) begin
+      $display("Set #%0d", set_iterator);
+      for (int line_iterator = 0; line_iterator < CACHE_WAY; line_iterator = line_iterator + 1) begin
+        tmp_line = sets[set_iterator][line_iterator];
+        tmp_line.data = 256;
+        $display("Line #%0d (%0d): %16b", line_iterator, set_iterator * CACHE_WAY + line_iterator, tmp_line.data);
+      end
+      $display();
+    end
+  end
+
+  always @(posedge RESET) begin
+    for (int set_iterator = 0; set_iterator < CACHE_SETS_COUNT; set_iterator = set_iterator + 1) begin
+      for (int line_iterator = 0; line_iterator < CACHE_WAY; line_iterator = line_iterator + 1) begin
+        tmp_line = sets[set_iterator][line_iterator];
+        tmp_line.reset();
+      end
+    end
   end
 endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,3 +1,9 @@
+`define reg_to_string(signal, value) \
+  if (signal !== value) begin \
+    $display("ASSERTION FAILED in %m: signal != value"); \
+    $finish; \
+  end
+
 `include "src/parameters.sv"
 `include "src/commands.sv"
 `include "src/cache.sv"
@@ -15,18 +21,19 @@ module test #(parameter _SEED = 225526);
   reg[7:0] ram[MEM_SIZE:0];
   integer memory_pointer = 0;
 
-  reg CLK;
-  reg RESET;
+  reg CLK = 0,
+      RESET = 0,
+      C_DUMP = 0;
   always #1 CLK = ~CLK;
 
   wire[ADDR1_BUS_SIZE-1:0] A1;
   wire[ADDR2_BUS_SIZE-1:0] A2;
   wire[DATA1_BUS_SIZE-1:0] D1;
   wire[DATA2_BUS_SIZE-1:0] D2;
-  wire[CTR1_BUS_SIZE-1:0] C1;
-  wire[CTR2_BUS_SIZE-1:0] C2;
+  wire[CTR1_BUS_SIZE-1 :0] C1;
+  wire[CTR2_BUS_SIZE-1 :0] C2;
 
-  Cache Cache_instance(CLK, A1, D1, C1, A2, D2, C2, RESET);
+  Cache Cache_instance(CLK, A1, D1, C1, A2, D2, C2, RESET, C_DUMP);
 
   initial begin
     // Memory initialization
@@ -41,9 +48,9 @@ module test #(parameter _SEED = 225526);
 //     $display();
 
     // Logic
-    CLK = 0;
-    RESET = 0;
-    $monitor("[%2t] CLK = %d", $time, CLK);
+    // $display("%0d", C2_WRITE_LINE);
+    #10 C_DUMP = 1;
+    // $monitor("[%2t] CLK = %d", $time, CLK);
     #20 $finish;
   end
 
