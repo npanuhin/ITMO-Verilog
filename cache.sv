231c35aec512c12fea7c735e36d517b7f944adb7
Nikita Panyuhin <n.panuhin@mail.ru>
2022-11-26 21:13:00 +0300

    Added initial solution

---
+++ cache.sv
@@ -0,0 +1,14 @@
+module Cache (
+  input CLK,
+  inout wire A1,
+  inout wire D1,
+  inout wire C1,
+  inout wire A2,
+  inout wire D2,
+  inout wire C2,
+  input RESET
+);
+  always @(RESET) begin
+
+  end
+endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,29 +1,78 @@
-`include "nand2.sv"
+`include "cache.sv"
 
-module nand2_test;
-  reg[1:0] in_value;
-  wire out;
-
-  nand2_switch not_instance(.out(out), .in1(in_value[1]), .in2(in_value[0]));
-
-  typedef enum {C1_NOP=0, C1_RESPONSE=7} enum_set;
-
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(1, nand2_test);
-
-    $monitor("in:%b, out:%b", in_value, out);
-
-    #1 in_value = 2'b00;
-    #2 in_value = 2'b01;
-    #3 in_value = 2'b10;
-    #4 in_value = 2'b11;
+`define assert(signal, value) \
+  if (signal !== value) begin \
+    $display("ASSERTION FAILED in %m: signal != value"); \
+    $finish; \
   end
 
+module test #(parameter _SEED = 225526);
+  integer SEED = _SEED;
+
+  // Given parameters
+  localparam MEM_SIZE = 512 * 1024;
+  localparam CACHE_WAY = 2;  // TODO
+  localparam CACHE_TAG_SIZE = 10;
+  localparam CACHE_LINE_SIZE = 16;
+  localparam CACHE_LINE_COUNT = 64;
+  // Calculated parameters
+  localparam CACHE_SIZE = -1;
+  localparam CACHE_SETS_COUNT = -1;
+  localparam CACHE_SET_SIZE = -1;
+  localparam CACHE_OFFSET_SIZE = -1;
+  localparam CACHE_ADDR_SIZE = -1;
+
+  // Commands defenition
+  typedef enum {
+    C1_NOP,
+    C1_READ8,
+    C1_READ16,
+    C1_READ32,
+    C1_INVALIDATE_LINE,
+    C1_WRITE8,
+    C1_WRITE16,
+    C1_WRITE32,
+    C1_RESPONSE
+  } C1_COMMANDS;
+  // Commands
+  typedef enum {
+    C2_NOP,
+    C2_READ_LINE,
+    C2_WRITE_LINE,
+    C2_RESPONSE
+  } C2_COMMANDS;
+
+  // Main
+  reg[7:0] ram[0:MEM_SIZE];
+  integer memory_pointer = 0;
+
+  reg CLK;
+  reg RESET;
+  always #1 CLK = ~CLK;
+
+  wire A1, D1, C1, A2, D2, C2;
+
+  Cache #(3) Cache_instance(CLK, A1, D1, C1, A2, D2, C2, RESET);
+
   initial begin
-    enum_set enum_var;
-    enum_var = C1_NOP; $display ("enum_var {C1_NOP} = %0d", enum_var);
-    enum_var = C1_RESPONSE; $display ("enum_var {C1_RESPONSE} = %0d", enum_var);
+    // Memory initialization
+    for (memory_pointer = 0; memory_pointer < MEM_SIZE; memory_pointer += 1) begin
+      ram[memory_pointer] = $random(SEED)>>16;
+    end
+
+//     $display("RAM:");
+//     for (memory_pointer = 0; memory_pointer < 100; memory_pointer += 1) begin
+//       $display("[%2d] %d", memory_pointer, ram[memory_pointer]);
+//     end
+//     $display();
+
+    // Logic
+    CLK = 0;
+    RESET = 0;
+    $monitor("[%2t] CLK = %d", $time, CLK);
+    #20 $finish;
   end
 
+//   always @(posedge CLK)
+//     $display("[%0t]\tCLK = %d", $time, CLK);
 endmodule
