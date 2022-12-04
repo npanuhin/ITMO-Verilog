c6c976d099d399c907ce6a65641d5fc3b2191e60
Nikita Panyuhin <n.panuhin@mail.ru>
2022-12-04 21:54:10 +0300

    Refactoring analytical solution

--- Analytical solution/solution.py
+++ Analytical solution/solution.py
@@ -1,14 +1,18 @@
 from parameters import *
 
-TIME = cache_hits = cache_misses = 0
+TIME = 0
+
+
+class CacheLine:
+    tag = None
+    LRU_bit = 0
+    valid = dirty = False
 
 
 class Cache:
     def __init__(self):
-        self.tags = [[None] * CACHE_WAY for _ in range(CACHE_SETS_COUNT)]
-        self.LRU_bit = [[0] * CACHE_WAY for _ in range(CACHE_SETS_COUNT)]
-        self.valid = [[0] * CACHE_WAY for _ in range(CACHE_SETS_COUNT)]
-        self.dirty = [[0] * CACHE_WAY for _ in range(CACHE_SETS_COUNT)]
+        self.lines = [[CacheLine() for _ in range(CACHE_WAY)] for _ in range(CACHE_SETS_COUNT)]
+        self.hits = self.misses = 0
 
         self.read8 = self.read16 = lambda addr: self.access(addr)
         self.write32 = lambda addr: self.access(addr, True)
@@ -20,14 +24,14 @@ class Cache:
 
     def read_line_from_MEM(self, tag, sset, line):
         global TIME
-        self.tags[sset][line] = tag
+        self.lines[sset][line].tag = tag
         self.wait_clk(1)
         TIME += MEM_CTR_DELAY
         for bbytes_start in range(0, CACHE_LINE_SIZE, 2):
             TIME += 1
         TIME -= 1
-        self.valid[sset][line] = 1
-        self.dirty[sset][line] = 0
+        self.lines[sset][line].valid = True
+        self.lines[sset][line].dirty = False
 
     def write_line_to_MEM(self):
         global TIME
@@ -35,35 +39,29 @@ class Cache:
         TIME += MEM_CTR_DELAY  # MemCTR
 
     def find_valid_line(self, tag, sset):
-        found_line = None
         for line in range(CACHE_WAY):
-            if self.valid[sset][line] == 1 and self.tags[sset][line] == tag:
-                found_line = line
-        return found_line
+            if self.lines[sset][line].valid and self.lines[sset][line].tag == tag:
+                return line
 
     def invalidate_line(self, sset, line):
-        if self.dirty[sset][line]:
+        if self.lines[sset][line].dirty:
             self.write_line_to_MEM()
-        self.valid[sset][line] = 0
+        self.lines[sset][line].valid = False
 
     def find_spare_line(self, sset):
         global TIME
-        found_line = None
         for line in range(CACHE_WAY):
-            if self.valid[sset][line] == 0:
-                found_line = line
+            if not self.lines[sset][line].valid:
+                TIME += 0.5
+                return line
 
-        if found_line is None:
-            for line in range(CACHE_WAY):
-                if self.LRU_bit[sset][line] == 0:
-                    found_line = line
-            self.invalidate_line(sset, found_line)
-        else:
-            TIME += 0.5
-        return found_line
+        for line in range(CACHE_WAY):
+            if self.lines[sset][line].LRU_bit == 0:
+                self.invalidate_line(sset, line)
+                return line
 
     def access(self, addr, is_write=False):
-        global TIME, cache_hits, cache_misses
+        global TIME
         self.wait_clk(0)  # To send command
         self.wait_clk(1)  # To receive command
         # req_offset = addr % (2 ** CACHE_OFFSET_SIZE)
@@ -75,20 +73,20 @@ class Cache:
         TIME += 0.5  # C1_NOP
 
         if found_line is None:
-            cache_misses += 1
+            self.misses += 1
             TIME += CACHE_MISS_DELAY - 2
             found_line = self.find_spare_line(req_set)
             TIME += 0.5
             self.read_line_from_MEM(req_tag, req_set, found_line)
         else:
-            cache_hits += 1
+            self.hits += 1
             TIME += CACHE_HIT_DELAY - 2.5
 
         if is_write:
-            self.dirty[req_set][found_line] = 1
+            self.lines[req_set][found_line].dirty = True
 
-        self.LRU_bit[req_set][found_line] = 1
-        self.LRU_bit[req_set][not found_line] = 0
+        self.lines[req_set][found_line].LRU_bit = 1
+        self.lines[req_set][not found_line].LRU_bit = 0
 
         TIME += 0.5  # C1_RESPONSE
         # В READ send_bytes не нужен, так как посылаем либо 8, либо 16 бит, одновременно с C1_RESPONSE
@@ -143,6 +141,6 @@ TIME += 1  # end of function
 
 print(f"Total time: {TIME} tacts")
 print("Cache hits: {}/{} = {}%".format(
-    cache_hits, cache_hits + cache_misses,
-    round(cache_hits * 100 / (cache_hits + cache_misses), 2) if cache_hits + cache_misses else 0
+    cache.hits, cache.hits + cache.misses,
+    round(cache.hits * 100 / (cache.hits + cache.misses), 2) if cache.hits + cache.misses else 0
 ))
